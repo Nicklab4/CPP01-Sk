4b1959ef47a3af7e6c382bc220cb105262e7b9b5
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2025-07-09 03:05:07 +0500

    Практическая работа 2.5, задание 2 - Калькулятор стоимости товара

--- CPP01-Sk-02-05-01/CPP01-Sk-02-05-01.cpp
+++ CPP01-Sk-02-05-01/CPP01-Sk-02-05-01.cpp
@@ -15,34 +15,34 @@ int main()
 {
 	std::system("chcp 1251");
 
-  int lap = 4; 
-  int speed = 358; 
-  int engine = 254; 
-  int wheels = 93; 
-  int steerinWheel = 49; 
-  int wind = 21; 
-  int rain = 17; 
-  std::cout << "===================\n"; 
-  std::cout << "Супер гонки. Круг " << lap << "\n"; 
-  std::cout << "===================\n"; 
-  std::cout << "Шумахер (" << speed << ")\n"; 
-  std::cout << "===================\n"; 
-  std::cout << "Водитель: Шумахер\n"; 
-  std::cout << "Скорость: " << speed << "\n"; 
-  std::cout << "-------------------\n"; 
-  std::cout << "Оснащение\n"; 
-  std::cout << "Двигатель: +" << engine << "\n"; 
-  std::cout << "Колеса: +" << wheels << "\n"; 
-  std::cout << "Руль: +" << steerinWheel << "\n"; 
-  std::cout << "-------------------\n"; 
-  std::cout << "Действия плохой погоды\n"; 
-  std::cout << "Ветер: -" << wind << "\n"; 
-  std::cout << "Дождь: -" << rain << "\n"; 
-  std::cout << "-------------------\n"; 
-   std::cout << "\n"; 
-  
-  speed = engine + speed + wheels + steerinWheel - wind - rain;
-  
-  std::cout << "===================\n"; 
-  std::cout << "Итоговоая скорость:" << speed << "\n"; 
+	int lap = 4; 
+	int speed = 358; 
+	int engine = 254; 
+	int wheels = 93; 
+	int steerinWheel = 49; 
+	int wind = 21; 
+	int rain = 17; 
+	std::cout << "===================\n"; 
+	std::cout << "Супер гонки. Круг " << lap << "\n"; 
+	std::cout << "===================\n"; 
+	std::cout << "Шумахер (" << speed << ")\n"; 
+	std::cout << "===================\n"; 
+	std::cout << "Водитель: Шумахер\n"; 
+	std::cout << "Скорость: " << speed << "\n"; 
+	std::cout << "-------------------\n"; 
+	std::cout << "Оснащение\n"; 
+	std::cout << "Двигатель: +" << engine << "\n"; 
+	std::cout << "Колеса: +" << wheels << "\n"; 
+	std::cout << "Руль: +" << steerinWheel << "\n"; 
+	std::cout << "-------------------\n"; 
+	std::cout << "Действия плохой погоды\n"; 
+	std::cout << "Ветер: -" << wind << "\n"; 
+	std::cout << "Дождь: -" << rain << "\n"; 
+	std::cout << "-------------------\n"; 
+	std::cout << "\n"; 
+
+	speed = engine + speed + wheels + steerinWheel - wind - rain;
+
+	std::cout << "===================\n"; 
+	std::cout << "Итоговоая скорость:" << speed << "\n"; 
 }
