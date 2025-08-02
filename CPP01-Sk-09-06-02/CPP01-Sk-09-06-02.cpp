63c164489db2e6f2dbea90ab05542950fd1b00a2
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2025-08-02 22:21:33 +0500

    Практическая работа 9.6. Задача 2

---
+++ CPP01-Sk-09-06-02/CPP01-Sk-09-06-02.cpp
@@ -0,0 +1,49 @@
+//Задача 2
+//Напишите программу, которая проверяет, верно ли, что данная строка представляет из
+//себя некоторую другую строку, повторённую несколько раз. Например, строка
+//dabudabudabu — это трижды повторённая строка dabu. Строка kapkap — это дважды
+//повторённая строка kap. А вот строку abdabdab или строку gogolmogol нельзя
+//представить как повторение некоторой другой строки.
+//
+//На вход программа должна принимать строку и выдавать ответ Yes, если строка является
+//повторением некоторой другой строки, и No, если это не так.
+
+#include <tchar.h>
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string str;
+	std::cin >> str;
+	int pos = 0;
+	std::string repeatWord;
+
+	for(int i = 1; i < str.length(); ++i) {
+		if(str[pos] == str[i]) {
+			repeatWord += str[i];
+			pos++;
+		}
+		else {
+			pos = 0;
+			repeatWord = "";
+		}
+	}
+
+	std::cout << repeatWord << std::endl;
+
+	bool isRepeat = true;
+
+	for(int i = 0, j = 0; i < str.size(); ++i, ++j) {
+		if(j == repeatWord.length())
+			j = 0;
+		if(str[i] != repeatWord[j]) {
+			isRepeat = false;
+			break;
+		}
+	}
+
+	if(isRepeat)
+		std::cout << "Yes\n";
+	else 
+		std::cout << "No\n";
+}
